fe4638690144958e7a5a82e31a6f2e9f269b37f1
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-10 00:55:23 +0100

    info.plist: aggiunte proprietà per richiedere accesso alla camera e alla photolibrary main.storyboard: aggiunta azione per il clic sull'icona della camera main.storyboard: aggiunta iboulet per la imageView viewController: creato oggetto imagePIcker di tipo imagePickerController e implementate sue proprietà viewController: implementata la funzione cameraTapped e imagePickerController

--- seeFood/ViewController.swift
+++ seeFood/ViewController.swift
@@ -15,10 +15,32 @@ import Vision
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        // -1- present()
+        // rende visibile in una certa maniera il controller in questione
+        // in questo caso imagepicker e lo spara a tutto schermo per default
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
