4f084c65e3adfbc32fa80c52478ad6c7ffe6f698
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-10 19:04:50 +0100

    ViewController: conversione immagine per CoreML ViewController: crea un oggetto model che usando Vision incorpora Inception3

--- seeFood/ViewController.swift
+++ seeFood/ViewController.swift
@@ -30,8 +30,20 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedImage
+            //convertiamo l'UIImage in un formato usabile da CoreML
+            guard (try? CIImage(image: userPickedImage)) != nil else {
+                fatalError("Conversion to CIImage failed.")
+            }
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image:CIImage) {
+        // assegnamo alla costante model l'oggetto mlmodel importato
+        // VNCoreMLModel viene da Vision che abbiamo importato all'inizio
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML failed.")
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
